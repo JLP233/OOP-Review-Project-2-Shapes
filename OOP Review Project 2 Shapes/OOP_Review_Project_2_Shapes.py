3973bbca7dbd81bf96fda38d595b721c80d8eecb
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-09-21 19:22:03 -0400

    added @property for both shape name and area with setter

--- OOP Review Project 2 Shapes/OOP_Review_Project_2_Shapes.py
+++ OOP Review Project 2 Shapes/OOP_Review_Project_2_Shapes.py
@@ -1,5 +1,20 @@
 
+#Basic Shape Class
 class BasicShape:
     def __init__(self, name="Shape"):
         self._name = name
         self._area = 0.0
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, n):
+        self._name = n
+
+    @property
+    def area(self):
+        return self._area
+
+
